ad16ae09bbc37db213e4306e4e22401f9d960203
Kim Hye Lee <kimh3418@naver.com>
2023-05-25 21:37:32 +0900

    i change

--- src/main/java/com/khr/project_movie/contrtollers/HomeController.java
+++ src/main/java/com/khr/project_movie/contrtollers/HomeController.java
@@ -1,9 +1,16 @@
 package com.khr.project_movie.contrtollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping(value="/")
 @Controller
 public class HomeController {
 
-
+    @RequestMapping(value="/" , method = RequestMethod.GET)
+    public ModelAndView getIndex(){
+        return new ModelAndView("home/index");
+    }
 }
