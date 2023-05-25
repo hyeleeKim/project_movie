3a5fb132d5744debcc95bc3ce87fe0222ecb9461
Kim Hye Lee <kimh3418@naver.com>
2023-05-25 21:15:02 +0900

    Initial commit

---
+++ src/main/java/com/khr/project_movie/ProjectMovieApplication.java
@@ -0,0 +1,13 @@
+package com.khr.project_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khr/project_movie/ProjectMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khr.project_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
