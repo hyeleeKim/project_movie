31769e865469b5f4dfa61b67447b0f0fd42cef4d
Kim Hye Lee <kimh3418@naver.com>
2023-06-09 23:03:07 +0900

    register finish

--- src/main/resources/static/resources/scripts/register.js
+++ src/main/resources/static/resources/scripts/register.js
@@ -2,10 +2,10 @@ const registerForm = document.getElementById('registerForm');
 
 
 // 회원가입 보여주기
-registerForm.show = () => {
-    registerForm.classList.remove('step-1', 'step-2', 'step-3', 'step-4');
-    registerForm.classList.add('step-1', 'visible');
-}
+// registerForm.show = () => {
+//     registerForm.classList.remove('step-1', 'step-2', 'step-3', 'step-4');
+//     registerForm.classList.add('step-1', 'visible');
+// }
 
 // 경고 보이게 하기
 function showWarning(element, text) {
@@ -56,12 +56,6 @@ registerForm.passwordCheckWarning.show = (text) => showWarning(registerForm.pass
 registerForm.passwordCheckWarning.hide = () => hideWarning(registerForm.passwordCheckWarning);
 
 
-registerForm.nicknameWarning = document.querySelector('[rel="nicknameWarning"]');
-registerForm.nicknameWarning.show = (text) => showWarning(registerForm.nicknameWarning, text);
-registerForm.nicknameWarning.hide = () => hideWarning(registerForm.nicknameWarning);
-
-
-
 // 회원가입 휴대폰 인증
 registerForm['cContactSend'].onclick = () => {
     registerForm.cContactWarning.hide();
@@ -72,7 +66,7 @@ registerForm['cContactSend'].onclick = () => {
         registerForm['cContact'].focus();
         return;
     }
-    if(!RegExp(/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/).test(registerForm['cContact'].value)) {
+    if (!new RegExp(/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/).test(registerForm['cContact'].value)) {
         registerForm.cContactWarning.show('올바른 전화번호를 입력해주세요.');
         registerForm['cContact'].focus();
         registerForm['cContact'].select();
@@ -81,52 +75,102 @@ registerForm['cContactSend'].onclick = () => {
 
     const xhr = new XMLHttpRequest();
     const formData = new FormData();
-    formData.append("contact",registerForm['cContact'].value);
-    xhr.open('POST','/sendContactCode');
-    xhr.onreadystatechange = () =>{
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status >= 200 && xhr.status < 400){
+    formData.append("contact", registerForm['cContact'].value);
+    xhr.open('POST', '/sendContactCode');
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status >= 200 && xhr.status < 400) {
                 const responseObject = JSON.parse(xhr.responseText);
-                switch(responseObject['result']){
+                switch (responseObject['result']) {
                     case 'success' :
-                        registerForm['cContact'].setAttribute('disabled','disabled');
-                        registerForm['cContactSend'].setAttribute('disabled','disabled');
+                        registerForm['cContact'].setAttribute('disabled', 'disabled');
+                        registerForm['cContactSend'].setAttribute('disabled', 'disabled');
                         registerForm['cContactCode'].removeAttribute('disabled');
                         registerForm['cContactVerify'].removeAttribute('disabled');
+                        registerForm['contactSalt'].value = responseObject.salt;
                         registerForm['cContactCode'].focus();
-                        registerForm.cContactWarning.show('입력하신 번호로 인증번호롤 전송했습니다. <br> 아래 확인란에 5분 이내로 입력해 주세요.');
+                        registerForm.cContactWarning.show('입력하신 번호로 인증번호로 전송했습니다. <br> 아래 인증번호 확인란에 5분 이내로 입력해 주세요.');
                         break;
                     case 'failure_duplicate':
                         registerForm.cContactWarning.show('이미 회원가입된 번호입니다. 가입하신 아이디로 로그인해주세요.<br>기억이 나지 않으시면 아이디 찾기를 이용해 주세요.');
+                        registerForm['cContact'].focus();
+                        registerForm['cContact'].select();
                         break;
                     default :
                         registerForm.cContactWarning.show('인증번호 전송에 실패했습니다. 잠시 후 다시 시도해 주세요.');
+                        registerForm['cContact'].focus();
                 }
             } else {
-              registerForm.cContactWarning.show('서버와 통신하지 못했습니다. 잠시 후 다시 시도해 주세요.');
+                registerForm.cContactWarning.show('서버와 통신하지 못했습니다. 잠시 후 다시 시도해 주세요.');
             }
         }
     };
     xhr.send(formData);
-    
-
-
-
 
 }
 
 
+// 회원가입 휴대폰 인증번호 확인
+registerForm['cContactVerify'].onclick = () => {
+    registerForm.cContactCodeWarning.hide();
 
+    if (registerForm['cContactCode'].value === '') {
+        registerForm.cContactCodeWarning.show('인증번호를 입력해 주세요.');
+        registerForm['cContactCode'].focus();
+        return;
+    }
+
+    if (!new RegExp(/^[0-9]{6}$/).test(registerForm['cContactCode'].value)) {
+        registerForm.cContactCodeWarning.show('올바른 인증번호를 입력해 주세요.');
+        registerForm['cContactCode'].focus();
+        registerForm['cContactCode'].select();
+        return;
+    }
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    formData.append('contact', registerForm['cContact'].value);
+    formData.append('code', registerForm['cContactCode'].value);
+    formData.append('salt', registerForm['contactSalt'].value);
+
+    xhr.open('PATCH', '/contactCode');
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const responseObject = JSON.parse(xhr.responseText);
+                switch (responseObject['result']) {
+                    case 'failure_expired':
+                        registerForm.cContactCodeWarning.show('해당 인증번호는 유효시간이 만료되었습니다. <br> 처음부터 다시 진행해 주세요');
+                        return;
+                    case 'success':
+                        registerForm.cContactCodeWarning.show('인증번호 인증이 완료되었습니다. <br> 다음 버튼을 눌러 회원가입을 진행해 주세요.');
+                        registerForm['next'].removeAttribute('disabled');
+                        return;
+                    case 'failure':
+                        registerForm.cContactCodeWarning.show('인증번호를 잘못 입력하셨습니다. <br> 다시한번 확인해 주세요.');
+                        return;
+                    default:
+                        registerForm.cContactCodeWarning.show('인증번호 확인에 실패했습니다. 잠시 후 다시 시도해 주세요.');
+                        registerForm['cContactCode'].focus();
+                }
+            } else {
+                registerForm.cContactCodeWarning.show('서버와 통신하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
+
+            }
+        }
+    };
+    xhr.send(formData);
+}
+
+// 회원가입 다음버튼 누르기
 registerForm.onsubmit = e => {
     e.preventDefault();
-
     registerForm.cContactWarning.hide();
     registerForm.cContactCodeWarning.hide();
     registerForm.nameWarning.hide();
     registerForm.emailWarning.hide();
     registerForm.passwordWarning.hide();
     registerForm.passwordCheckWarning.hide();
-    registerForm.nicknameWarning.hide();
 
     if (registerForm.classList.contains('step-1')) {
 
@@ -135,18 +179,33 @@ registerForm.onsubmit = e => {
             registerForm['cContact'].focus();
             return;
         }
-        if(!RegExp(/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/).test(registerForm['cContact'].value)){
+        if (!new RegExp(/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/).test(registerForm['cContact'].value)) {
             registerForm.cContactWarning.show('올바른 전화번호를 입력해주세요.');
             return;
         }
+
         if (registerForm['cContactCode'].value === '') {
-            registerForm.cContactCodeWarning.show('인증번호를 입력해 주세요.');
+            registerForm.cContactCodeWarning.show('인증번호 전송 및 6자리 숫자를 입력해 주세요.');
+            registerForm['cContactCode'].focus();
+            return;
+        }
+
+        if (!new RegExp(/^[0-9]{6}$/).test(registerForm['cContactCode'].value)) {
+            registerForm.cContactCodeWarning.show('올바른 인증번호를 입력해 주세요.');
+            registerForm['cContactCode'].focus();
+            registerForm['cContactCode'].select();
+            return;
+        }
+
+        if (registerForm['contactSalt'].value === '') {
+            registerForm.cContactCodeWarning.show('인증번호 확인을 진행해 주세요.');
             registerForm['cContactCode'].focus();
             return;
         }
 
         registerForm.classList.remove('step-1');
         registerForm.classList.add('step-2');
+
     } else if (registerForm.classList.contains('step-2')) {
         registerForm.agreeWarning.hide();
         if (!registerForm['agreeService'].checked) {
@@ -162,19 +221,55 @@ registerForm.onsubmit = e => {
         registerForm.classList.remove('step-2');
         registerForm.classList.add('step-3');
     } else if (registerForm.classList.contains('step-3')) {
+        registerForm.nameWarning.hide();
+        registerForm.birthWarning.hide();
+        registerForm.emailWarning.hide();
+        registerForm.passwordWarning.hide();
+        registerForm.passwordCheckWarning.hide();
+
         if (registerForm['name'].value === '') {
             registerForm.nameWarning.show('이름을 입력해 주세요.');
             registerForm['name'].focus();
             return;
         }
+        if (!new RegExp(/^[가-힣]{2,4}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/).test(registerForm['name'].value)) {
+            registerForm.nameWarning.show('이름을 정확하게 입력해 주세요.');
+            registerForm['name'].focus();
+            registerForm['name'].select();
+            return;
+        }
+
         if (registerForm['birth'].value === '') {
             registerForm.birthWarning.show('생일을 입력해 주세요.');
             registerForm['birth'].focus();
             return;
         }
+
+        if (registerForm['birth'].value > formatDate(new Date())) {
+            registerForm.birthWarning.show('정확한 생년월일을 입력해주세요.');
+            registerForm['birth'].focus();
+            registerForm['birth'].select();
+            return;
+        }
+
+        if (!new RegExp(/^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/).test(registerForm['birth'].value)) {
+            registerForm.birthWarning.show('형식에 맞는 생년월일을 입력해 주세요.');
+            registerForm['birth'].focus();
+            registerForm['birth'].select();
+            return;
+        }
+
         if (registerForm['email'].value === '') {
             registerForm.emailWarning.show('이메일을 입력해 주세요.')
             registerForm['email'].focus();
+            return;
+        }
+
+        if (!new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(registerForm['email'].value)) {
+            registerForm.emailWarning.show('잘못된 이메일 형식입니다. 다시 한번 확인해 주세요.');
+            registerForm['email'].focus();
+            registerForm['email'].select();
+
             return;
         }
         if (registerForm['password'].value === '') {
@@ -182,18 +277,61 @@ registerForm.onsubmit = e => {
             registerForm['password'].focus();
             return;
         }
+
+        if (!new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/).test(registerForm['password'].value)) {
+
+            registerForm.passwordWarning.show('형식에 맞게 비밀번호를 적어주세요. <br> 대문자+소문자+숫자+특수문자 1개이상포함 8~20자리');
+            registerForm['password'].focus();
+            registerForm['password'].select();
+            return;
+        }
+
         if (registerForm['passwordCheck'].value === '') {
             registerForm.passwordCheckWarning.show('비밀번호를 다시 한번 입력해 주세요.');
             registerForm['passwordCheck'].focus();
             return;
         }
-        if (registerForm['nickname'].value === '') {
-            registerForm.nicknameWarning.show('닉네임을 입력해 주세요');
-            registerForm['nickname'].focus();
+
+        if (registerForm['passwordCheck'].value !== registerForm['password'].value) {
+            registerForm.passwordCheckWarning.show('비밀번호가 동일하지 않습니다. 다시 한번 확인해 주세요.');
+            registerForm['passwordCheck'].focus();
+            registerForm['passwordCheck'].select();
             return;
         }
 
-        registerForm.classList.remove('step-3');
-        registerForm.classList.add('step-4');
+
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+        formData.append('name', registerForm['name'].value);
+        formData.append('birthStr', registerForm['birth'].value);
+        formData.append('email', registerForm['email'].value);
+        formData.append('password', registerForm['password'].value);
+        formData.append('contact',registerForm['cContact'].value);
+        xhr.open('POST', './register');
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    const responseObject = JSON.parse(xhr.responseText);
+                    switch (responseObject.result) {
+                        case 'failure':
+                            registerForm.passwordCheckWarning.show('회원가입에 실패했습니다. 잠시 후 다시 시도해 주세요.');
+                            break;
+                        case 'failure_email_duplicate':
+                            registerForm.emailWarning.show('이미 사용중인 이메일입니다.');
+                            break;
+                        default:
+                        case 'success':
+                            registerForm.classList.remove('step-3');
+                            registerForm.classList.add('step-4');
+                            break;
+                    }
+                } else {
+                    registerForm.passwordCheckWarning.show('서버와 통신하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
+                }
+            }
+        };
+        xhr.send(formData);
+
+
     }
 }
